eace4b7411677bd759ffdf636d1bfdb1f05e3a12
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-01-21 17:45:20 +0300

    product total

--- app.js
+++ app.js
@@ -5,23 +5,27 @@
  
     if (e.target.className === "fa-solid fa-plus") {
       e.target.previousElementSibling.innerText++;
+      productTotal(e.target)
     }
      else if (e.target.className === "fa-solid fa-minus") {
       if (e.target.nextElementSibling.innerText > 1) {
        e.target.nextElementSibling.innerText--;
+       productTotal(e.target)
       } 
     }
   
     else if(e.target.className ==="remove"){
      
        e.target.closest(".box").remove()
+       
     }
-  
+    
   });
 
 const productTotal =(eleman)=>{
-   const number =document.querySelector(".num").innerText;
-   const price =document.querySelector("strong").innerText;
-   const productTotal= number * price;
-    
+   const number =eleman.parentElement.querySelector(".num").innerText;
+   const price =eleman.parentElement.parentElement.querySelector("strong").innerText;
+   const productTotal= (number * price).toFixed(2);
+
+   eleman.parentElement.parentElement.querySelector(".product-span").innerText = productTotal; 
 }
